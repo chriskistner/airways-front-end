9dcacfc6bd5f021dfb48d4469665938133a2e46d
Christopher Kistner <chris.kistner0@gmail.com>
2019-02-19 17:41:43 -0800

    Modified user state to track original address info

--- src/reducers/authentication.js
+++ src/reducers/authentication.js
@@ -6,6 +6,9 @@ const initialState = {
     userId: null,
     pending: true,
     userName: '',
+    address: '',
+    city: '',
+    state: '',
     zipLat: null,
     zipLong: null,
     errors: false,
@@ -17,7 +20,7 @@ export default function auth (state = initialState, action) {
   switch(action.type) {
       case SET_AUTHENTICATION:
         const id = action.payload !== null ? action.payload.id : null;
-        return {...state, userId: id, pending: false, zipLat: null, zipLong: null}
+        return {...state, userId: id, pending: false, address: '', ciy: '', state: '', zipLat: null, zipLong: null}
 
       case SET_LOGIN_ERROR:
         const errorState = action.payload;
@@ -25,10 +28,12 @@ export default function auth (state = initialState, action) {
         
       case SET_USER_DATA:
         const userName = action.payload.user_name;
-        const userZip = action.payload.zip_code;
+        const userAddress = action.payload.address;
+        const userCity = action.payload.city;
+        const userState = action.payload.state
         const userLat = action.payload.home_latitude;
         const userLong = action.payload.home_longitude;
-        return {...state, userName: userName, userZip: userZip, zipLat: userLat, zipLong: userLong }
+        return {...state, userName: userName, address: userAddress, city: userCity, state: userState, zipLat: userLat, zipLong: userLong }
 
       case GET_GEOCODE:
         const lat = action.payload.geometry.location.lat;
